db776e2781d762d6fea1f3a193b5a603c0dad4c4
Abaid Ali Miran <aabaid737@gmail.com>
2023-07-20 21:45:50 +0500

    Create area.c
    This program assists you in finding the area of circle

---
+++ Calculation Programs in C/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+	float pi=3.14;
+	float r ;
+	printf("Enter Radius: ");
+	scanf("%f",&r) ;
+	float a =pi*r*r;
+	printf("Area of Circle is %f", a) ;
+
+}
