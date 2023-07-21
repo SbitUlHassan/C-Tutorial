a6a43dc591d8b2208d3b2c74226a275acc2ad0ea
Abaid Ali Miran <aabaid737@gmail.com>
2023-07-21 20:59:45 +0500

    Type Casting

---
+++ Type Casting/ty1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+	int i=17;
+	char c = 'c';
+	int sum;
+	sum=i+c;
+	printf("The value is %d",sum);
+}
---
+++ Type Casting/ty2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+	int i=17;
+	int c=5;
+	double mean;
+	mean=(double)i/c;
+	printf("The value is %f",mean);
+}
